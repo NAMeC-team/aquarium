9051394db1981474a15effc60acf208164145528
Thomas Wanchai MENIER <wanchai.menier@gmail.com>
2025-01-06 19:54:45 +0100

    feat: match protocol version with CRAbE, alert if mismatch

--- src/slices/crabeSlice.ts
+++ src/slices/crabeSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
-import { ToolMessage } from "../types/message"
+import { TOOL_MESSAGE_PROTO_VER, ToolMessage } from "../types/message"
 import { convertArrayToObject, parseWorld } from "../helpers/parser"
 import { CrabeState } from "../types/state"
 export const crabeSlice = createSlice({
@@ -8,7 +8,15 @@ export const crabeSlice = createSlice({
   reducers: {
     receiveData(state, action: PayloadAction<ToolMessage>) {
       console.log(action.payload)
+      if (action.payload.version !== TOOL_MESSAGE_PROTO_VER) {
+        alert(
+          `Protocol version mismatch !\n
+          Viewer uses version ${TOOL_MESSAGE_PROTO_VER}, CRAbE uses ${action.payload.version}\n
+          Subsequent code will most probably break, check new version at the CRAbE repository.`,
+        )
+      }
       return {
+        version: action.payload.version,
         world: parseWorld(action.payload.world),
         annotations: convertArrayToObject(action.payload.data.annotations),
       }
